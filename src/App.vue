36b773806e6ae2ed9c4469313876291c6a5077e5
chenyn <786035415@qq.com>
2018-11-08 20:53:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!--<div class="header">-->
+      <header></header>
+    <!--</div>-->
+    <div class="tab">
+      I am tab!
+    </div>
+    <div class="content">
+      I am content!
+    </div>
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header';
+  export default {
+    components: {
+      'header': header
+    }
+  };
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
